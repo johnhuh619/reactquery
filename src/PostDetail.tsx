267c64d141e8c5d62dec5566d46a2ca092e6de70
JaeoneHeo <jaewonheo666@gmail.com>
2023-08-15 19:19:17 +0900

    mutation을 이용한 RUD

--- src/PostDetail.tsx
+++ src/PostDetail.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useQuery } from 'react-query';
+import { useQuery, useMutation } from 'react-query';
 import { IPost } from './Posts';
 interface IPostDetailProps {
   post: IPost;
@@ -16,15 +16,52 @@ async function fetchComments(postId: number) {
   );
   return response.json();
 }
-
+async function deletePost(postId: number){
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/postId=${postId}`,
+    {method: "DELETE"}
+  );
+  return response.json();
+}
+async function updataePost(postId:number) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/postId=${postId}`,
+    {
+      method: 'PATCH',
+      body: JSON.stringify({ title: "react query 업데이트 입니다!"})
+    }
+  );
+  return response.json();
+}
 export function PostDetail({ post }: IPostDetailProps) {
   // replace with useQuery
   const {data, isLoading, isError}  = useQuery<IComments[]>(['comments',post.id], () => fetchComments(post.id));
  // fetchComments는 postId를 인자로 받기 때문에 익명함수로 감싸서 작성한다
+ const deleteMutation = useMutation((postId :number) => deletePost(postId));
+ const updateMutation = useMutation((postId :number) => updataePost(postId));
   return (
     <>
       <h3 style={{ color: "blue" }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
+      <button onClick={()=>deleteMutation.mutate(post.id)}>Delete</button>
+      <button onClick={()=>updateMutation.mutate(post.id)}>Update title</button>
+      {deleteMutation.isError && (
+        <div style={{color:'red'}}>ERROR</div>
+      )}
+      {deleteMutation.isLoading && (
+        <div style={{color:'blue'}}>deleting post...</div>
+      )}
+      {deleteMutation.isSuccess && (
+        <div style={{color:'green'}}>success delete post</div>
+      )}
+      {updateMutation.isError && (
+        <div style={{color:'red'}}>ERROR</div>
+      )}
+      {updateMutation.isLoading && (
+        <div style={{color:'blue'}}>updating post...</div>
+      )}
+      {updateMutation.isSuccess && (
+        <div style={{color:'green'}}>success update post</div>
+      )}
       <p>{post.body}</p>
       <h4>Comments</h4>
       {
